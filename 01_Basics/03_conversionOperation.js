1a7583fd6e84a740b736f5dfb700599d743f3b45
Manas <105148415+Manas0726@users.noreply.github.com>
2023-10-06 07:02:34 +0000

    conversion of data types

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,27 @@
+let age = "12"
+
+console.log(typeof age);
+
+let toInteger = Number(age);
+
+console.log(typeof toInteger);
+
+console.log(toInteger) 
+
+// "33" => 33
+//  null => 0
+//  undefined => NaN
+//  "33abc" => NaN
+//  true => 1 false => 0
+
+
+let isLoggedIn = true;
+
+console.log(typeof isLoggedIn);
+
+let val = Number(isLoggedIn);
+
+console.log(val);
+
+
+console.log(Boolean(age))
