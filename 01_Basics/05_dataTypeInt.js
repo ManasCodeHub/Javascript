530bb1a1734af7fbb2fded4d75babe6f7916d344
Manas <105148415+Manas0726@users.noreply.github.com>
2023-10-08 19:35:23 +0000

    data types return value

--- 01_Basics/05_dataTypeInt.js
+++ 01_Basics/05_dataTypeInt.js
@@ -15,6 +15,21 @@
             Array
             Objects
             Functions
+
+    1) Primitive Datatypes
+       Number => number
+       String  => string
+       Boolean  => boolean
+       null  => object
+       undefined  =>  undefined
+       Symbol  =>  symbol
+       BigInt  =>  bigint
+
+    2) Non-primitive Datatypes
+       Arrays  =>  object
+       Function  =>  function
+       Object  =>  object
+
 */
 
 //Symbol 
