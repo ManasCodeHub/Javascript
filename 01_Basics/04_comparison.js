379d727334c0869a94985c9accd4d7d30842979a
Manas <105148415+Manas0726@users.noreply.github.com>
2023-10-08 19:14:37 +0000

    comparison

---
+++ 01_Basics/04_comparison.js
@@ -0,0 +1,9 @@
+
+// console.log("2" > 1); // string ignored 
+
+// console.log(null > 0)
+// console.log(null < 0)
+// console.log(null == 0)
+// console.log(null <= 0)
+
+// console.log("10"===10); // value and type is checked
